271bac107addd60008fc67ff0297a6ecc45a259e
aizwellenstan <aizwellenstan@gmail.com>
2019-11-19 18:31:26 +0900

    cors

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,11 @@
 class HomeController < ApplicationController
-    after_action :allow_foo_iframe
+    def iframe_action
+        response.headers.delete "X-Frame-Options"
+    end
+    def index
+        response.headers.delete "X-Frame-Options"
+    end
 
-  FOO_URL = "*"
-  def index
-  end
-
-  private
   
-
-  def allow_foo_iframe
-    response.headers['X-Frame-Options'] = "ALLOW-FROM #{FOO_URL}"
-    response.headers['Content-Security-Policy'] = "frame-ancestors #{FOO_URL}"
-  end
     
 end
