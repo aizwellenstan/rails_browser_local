9f9372627a88a34d28fdf3010fe9e1fd089df523
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 02:16:24 +0900

    add src

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,5 @@
 class HomeController < ApplicationController
-    def iframe_action
-        response.headers.delete "X-Frame-Options"
-    end
     def index
         response.headers.delete "X-Frame-Options"
     end
-
-  
-    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root "home#index"
+  # root "home#index"
+  get '/', :to => redirect('/dist/index.html')
 end
