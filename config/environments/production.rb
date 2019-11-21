759e043f0a60fe14caf293a80b813cf45ebfce6b
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 16:50:32 +0900

    rails_vue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -79,6 +80,7 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_controller.relative_url_root = '/memberpage'
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
