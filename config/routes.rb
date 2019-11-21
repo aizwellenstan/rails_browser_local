18293914ce23353676baa8e122288f70844e935a
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 10:40:15 +0900

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   # root "home#index"
-  get '/', :to => redirect('/dist/index.html')
+  get '/', :to => redirect('/index.html')
 end
