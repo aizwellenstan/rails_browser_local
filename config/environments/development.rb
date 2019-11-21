da58f12791aaeefabce8a1598d1dfa09631f504e
aizwellenstan <aizwellenstan@gmail.com>
2019-11-21 16:57:57 +0900

    rails_vue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,7 +52,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_controller.relative_url_root = '/memberpage'
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,6 @@ Rails.application.configure do
   # and those relying on copy on write to perform better.
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
-  
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
@@ -80,7 +79,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_controller.relative_url_root = '/memberpage'
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
